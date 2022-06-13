9850bb18eb797f69c01af53b92a48e0a399b17ef
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-06-12 20:17:15 -0400

    Create Auto Kick Rejoin.lua

---
+++ scripts/Auto Kick Rejoin.lua
@@ -0,0 +1,12 @@
+--This script makes it so that whenever you get kicked you will rejoin the same server
+
+local TeleportService = game:GetService("TeleportService")
+local ClientReplicator = game:GetService("NetworkClient").ClientReplicator
+local CurrentServer = game["JobId"]
+
+ClientReplicator.AncestryChanged:Connect(function()
+    TeleportService:TeleportToPlaceInstance(game["PlaceId"], CurrentServer)
+end)
+
+--Line 10 is for testing
+--game:GetService("Players").LocalPlayer:Kick()
