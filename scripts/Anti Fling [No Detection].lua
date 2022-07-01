08ca8618cae08d3926e53544908c7e689cd5b773
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-07-01 17:24:07 -0400

    Update and rename Anti Fling no detection.lua to Anti Fling [No Detection].lua

--- scripts/Anti Fling [No Detection].lua
+++ scripts/Anti Fling [No Detection].lua
@@ -1,5 +1,6 @@
 -- Made in 2 minutes, don't feel like improving cuz lazy
 -- This isn't detected on most games, unless they have some shitty anti cheat that detects you setting velocity on other players
+
 while true do
     for _,x in next, game:GetService("Players"):GetPlayers() do
         if x and x ~= game:GetService("Players").LocalPlayer and x.Character then
