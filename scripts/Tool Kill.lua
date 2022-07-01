9ef70dce890df57c45aa494bdc3e58d131b32afe
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-07-01 17:20:18 -0400

    Update Tool Kill.lua

--- scripts/Tool Kill.lua
+++ scripts/Tool Kill.lua
@@ -1,114 +1,121 @@
--- One of my more older script remakes
+-- This is my best Tool Kill script (outdated but still works!)
+local Target = [[ AnthonyIsntHere ]]
 
-local Target = "Anthony"
+local LoopKill = false
 
 local function GetPlayer(Name)
-    Name = Name:lower()
-    for _,x in next, game:GetService("Players"):GetPlayers() do
-        if string.lower(x.Name):find(Name) == 1 then
-            return x
+    local Players = game:GetService("Players");
+    local LocalPlayer = Players.LocalPlayer;
+    Name = Name:lower():gsub(" ","")
+    for _,x in next, Players:GetPlayers() do
+        if x ~= LocalPlayer then
+            if x.Name:lower():match("^"..Name) then
+                return x;
+            elseif x.DisplayName:lower():match("^"..Name) then
+                return x;
+            end
         end
     end
 end
+
 local function Message(MTitle,MText,Time)
-    game:GetService("StarterGui"):SetCore("SendNotification",{Title = MTitle;Text = MText;Duration = Time;})
+    game:GetService("StarterGui"):SetCore("SendNotification",{Title = MTitle, Text = MText, Icon = "rbxassetid://2541869220", Duration = Time}) 
 end
 
-if not GetPlayer(Target) then
-    return Message("Error",">   Player does not exist.",5)
+local function Kill()
+    if not GetPlayer(Target) then
+        return Message("Error",">   Player does not exist.",5)
+    end
+    
+    repeat game:GetService("RunService").Heartbeat:wait() until GetPlayer(Target).Character and GetPlayer(Target).Character:FindFirstChildOfClass("Humanoid") and GetPlayer(Target).Character:FindFirstChildOfClass("Humanoid").Health > 0
+    local Player = game:GetService("Players").LocalPlayer
+    local Character
+    local Humanoid
+    local RootPart
+    local Tool
+    local Handle
+    
+    local TPlayer = GetPlayer(Target)
+    local TCharacter = TPlayer.Character
+    local THumanoid
+    local TRootPart
+    
+    if Player.Character ~= nil and Player.Character and Player.Character.Name == Player.Name then
+        Character = Player.Character
+    else
+        return Message("Error",">   Missing Character")
+    end
+    if Character:FindFirstChildOfClass("Humanoid") then
+        Humanoid = Character:FindFirstChildOfClass("Humanoid")
+    else
+        return Message("Error",">   Missing Humanoid")
+    end
+    if Humanoid and Humanoid.RootPart then
+        RootPart = Humanoid.RootPart
+    else
+        return Message("Error",">   Missing RootPart")
+    end
+    if Character:FindFirstChildOfClass("Tool") then
+        Tool = Character:FindFirstChildOfClass("Tool")
+    elseif Player.Backpack:FindFirstChildOfClass("Tool") and Humanoid then
+        Tool = Player.Backpack:FindFirstChildOfClass("Tool")
+        Humanoid:EquipTool(Player.Backpack:FindFirstChildOfClass("Tool"))
+    else
+        return Message("Error",">   Missing Tool")
+    end
+    if Tool and Tool:FindFirstChild("Handle") then
+        Handle = Tool.Handle
+    else
+        return Message("Error",">   Missing Tool's Handle")
+    end
+    
+    --Target
+    if TCharacter:FindFirstChildOfClass("Humanoid") then
+        THumanoid = TCharacter:FindFirstChildOfClass("Humanoid")
+    else
+        return Message("Error",">   Missing Target Humanoid")
+    end
+    if THumanoid.RootPart then
+        TRootPart = THumanoid.RootPart
+    else
+        return Message("Error",">   Missing Target RootPart")
+    end
+    
+    if THumanoid.Sit then
+        return Message("Error",">   Target is seated")
+    end
+    
+    local OldCFrame = RootPart.CFrame
+    
+    Humanoid:Destroy()
+    local NewHumanoid = Humanoid:Clone()
+    NewHumanoid.Parent = Character
+    NewHumanoid:UnequipTools()
+    NewHumanoid:EquipTool(Tool)
+    Tool.Parent = workspace
+
+    local Timer = os.time()
+
+    repeat
+        if (TRootPart.CFrame.p - RootPart.CFrame.p).Magnitude < 500 then
+            Tool.Grip = CFrame.new()
+            Tool.Grip = Handle.CFrame:ToObjectSpace(TRootPart.CFrame):Inverse()
+        end
+        firetouchinterest(Handle,TRootPart,0)
+        firetouchinterest(Handle,TRootPart,1)
+        game:GetService("RunService").Heartbeat:wait()
+    until Tool.Parent ~= Character or not TPlayer or not TRootPart or THumanoid.Health <= 0 or os.time() > Timer + .20
+    Player.Character = nil
+    NewHumanoid.Health = 0
+    Player.CharacterAdded:wait()
+    repeat game:GetService("RunService").Heartbeat:wait() until Player.Character:FindFirstChild("HumanoidRootPart")
+    Player.Character.HumanoidRootPart.CFrame = OldCFrame
 end
 
-repeat game:GetService("RunService").Heartbeat:wait() until GetPlayer(Target).Character and  GetPlayer(Target).Character:FindFirstChildOfClass("Humanoid").Health > 0 or not GetPlayer(Target)
-
-local Player = game:GetService("Players").LocalPlayer
-local Character = Player.Character
-local Humanoid
-local HumanoidRootPart
-local Torso
-local Tool
-local Handle
-
-local TPlayer = GetPlayer(Target)
-local TCharacter = TPlayer.Character
-local THumanoid
-local THumanoidRootPart
-local TTorso
-
-if Character:FindFirstChild("HumanoidRootPart") then
-    HumanoidRootPart = Character.HumanoidRootPart
-end
-if Character:FindFirstChild("Torso") then
-    Torso = Character.Torso
-end
-if Character:FindFirstChildOfClass("Humanoid") then
-    Humanoid = Character:FindFirstChildOfClass("Humanoid")
-end
-if Character:FindFirstChildOfClass("Tool") then
-    Tool = Character:FindFirstChildOfClass("Tool")
-elseif Player.Backpack:FindFirstChildOfClass("Tool") and Humanoid then
-    Tool = Player.Backpack:FindFirstChildOfClass("Tool")
-    Humanoid:EquipTool(Player.Backpack:FindFirstChildOfClass("Tool"))
-end
-if Tool and Tool:FindFirstChild("Handle") then
-    Handle = Tool.Handle
-end
-if TCharacter:FindFirstChild("HumanoidRootPart") then
-    THumanoidRootPart = TCharacter.HumanoidRootPart
-end
-if TCharacter:FindFirstChild("Torso") then
-    TTorso = TCharacter.Torso
-elseif TCharacter:FindFirstChild("UpperTorso") then
-    TTorso = TCharacter.UpperTorso
-end
-if TCharacter:FindFirstChildOfClass("Humanoid") then
-    THumanoid = Character:FindFirstChildOfClass("Humanoid")
-end
-if not HumanoidRootPart then
-    Message("Error",">   Missing HumanoidRootPart, Trying Torso.",5)
-    wait(1)
-    if Torso then
-        Message("Succes",">   Torso found.",5)
-        wait(1)
-        else Message("Error",">   Missing Torso") return
+if not LoopKill then
+    Kill()
+else
+    while true do
+        Kill()
     end
 end
-if Humanoid.RigType == Enum.HumanoidRigType.R15 then
-    Message("Error",">   Must be R6 to use this script.",5)
-    wait(1)
-    return
-end
-if not Tool then
-    Message("Error",">   You have no tools.",5)
-    wait(1)
-    return
-end
-if not Handle then
-    Message("Error",">   Tool doesn't have handle.",5)
-    wait(1)
-    return
-end
-if not THumanoidRootPart then
-    Message("Error",">   Missing HumanoidRootPart on Target, Trying Torso.",5)
-    wait(1)
-    if Torso then
-        Message("Succes",">   Torso found on Target.",5)
-        wait(1)
-        else Message("Error",">   Missing Torso on Target",5) return
-    end
-end
-
-Humanoid:Destroy()
-local NewHumanoid = Instance.new("Humanoid",Character)
-NewHumanoid:UnequipTools()
-NewHumanoid:EquipTool(Tool)
-Tool.Parent = workspace
-
-repeat
-    Tool.Grip = CFrame.new()
-    Tool.Grip = Handle.CFrame:ToObjectSpace(TTorso.CFrame):Inverse()
-    firetouchinterest(Handle,TTorso,0)
-    firetouchinterest(Handle,TTorso,1)
-    game:GetService("RunService").Heartbeat:wait()
-until Tool.Parent ~= Character
-Player.Character = nil
-NewHumanoid.Health = 0
