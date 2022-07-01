57c194a004be92122f0d971ff238046799d35a8a
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-07-01 17:19:20 -0400

    Create [R15] Rescale.lua

---
+++ scripts/[R15] Rescale.lua
@@ -0,0 +1,47 @@
+-- This scripts manipulates originalsize and originalposition values inside of your character
+-- This script is for r15
+-- Use settings table to mess around with types of weird scaling
+
+local Settings = {
+    Values = {
+        BodyTypeScale = true;
+        BodyProportionScale = false;
+        BodyWidthScale = true;
+        BodyHeightScale = false;
+        BodyDepthScale = true;
+        HeadScale = true;
+    };
+    OriginalSize = true;
+    OriginalPosition = true;
+}
+
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local Character = Player.Character
+local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+local WaitFunc = function(x) x.DescendantAdded:wait() task.wait() end
+
+if Humanoid.RigType == Enum.HumanoidRigType.R6 then return end
+
+for _,x in next, Settings.Values do
+    if x then
+        if Settings.OriginalSize then --Specific Path/Settings
+            for _, _os in next, Character:GetDescendants() do
+                if _os.Name == "OriginalSize" and _os:IsA("ValueBase") then
+                    _os:Destroy()
+                end
+            end
+        end
+        if Settings.OriginalPosition then
+            for _, _op in next, Character:GetDescendants() do
+                if _op.Name == "OriginalPosition" and _op:IsA("ValueBase") then
+                    _op:Destroy()
+                end
+            end
+        end
+        if Humanoid:FindFirstChild(tostring(_)) then
+            Humanoid:FindFirstChild(tostring(_)):Destroy()
+        end
+        WaitFunc(Character)
+    end
+end
