5390a7993cb470c04c43f9f1352b5bfaf08b7bc5
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-07-08 13:55:38 -0400

    Create Parent Tools to a Tool.lua

---
+++ scripts/Parent Tools to a Tool.lua
@@ -0,0 +1,21 @@
+-- Requires two or more tools
+-- Equip tool of choice and execute; will parent tools to the tool of choice.
+
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer
+local Character = Player.Character
+
+local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+local Tool = Character:FindFirstChildWhichIsA("Tool") or Player.Backpack:FindFirstChildWhichIsA("Tool")
+
+Humanoid:UnequipTools()
+
+for _, x in next, Player.Backpack:GetChildren() do
+    if _ > 0 then
+        x.Parent = Character
+        x.Parent = Tool
+        x.Parent = Player.Backpack
+        x.Parent = Tool
+    end
+end
