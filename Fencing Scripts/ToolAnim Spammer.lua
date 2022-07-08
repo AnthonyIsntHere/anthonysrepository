0db2b227827e115a46d987ae15c8cd3aebdcf450
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-07-08 12:49:16 -0400

    Create ToolAnim Spammer.lua

---
+++ Fencing Scripts/ToolAnim Spammer.lua
@@ -0,0 +1,31 @@
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer
+local Character = Player.Character
+
+local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+
+Humanoid:UnequipTools()
+
+for _, x in next, Player.Backpack:GetChildren() do
+    if x:IsA("Tool") and tostring(x):lower():match("foil") then
+        x.Parent = Character
+    end
+end
+
+for i = 1, 2 do
+    for _, x in next, Characterr:GetChildren() do
+        if x:IsA("Tool") and tostring(x):lower():match("foil") then
+            local handle = x:FindFirstChild("Handle")
+
+            x.RequiresHandle = false
+
+            if handle then
+                handle.Name = "deez nuts"
+            end
+
+            x:Activate()
+        end
+    end
+    wait()
+end
