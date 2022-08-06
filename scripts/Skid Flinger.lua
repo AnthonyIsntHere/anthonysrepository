328d01a9e349df9b41ee7ba385bf551e6e3123a3
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-08-06 12:47:31 -0400

    Update Skid Flinger.lua

--- scripts/Skid Flinger.lua
+++ scripts/Skid Flinger.lua
@@ -1,6 +1,7 @@
 -- Made by AnthonyIsntHere (got leaked and skids are claiming it as their own LOL)
+-- Now supports r15 originalsize rescale!
 
-local Targets = {"AnthonyIsntHere"} -- "All", "Target Name", "arian_was_here"
+local Targets = {"AnthonyIsntHere", "ArianBlaack"} -- "All", "Target Name", "arian_was_here"
 
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
@@ -76,20 +77,23 @@ local SkidFling = function(TargetPlayer)
         if not TCharacter:FindFirstChildWhichIsA("BasePart") then
             return
         end
+        
         local FPos = function(BasePart, Pos, Ang)
             RootPart.CFrame = CFrame.new(BasePart.Position) * Pos * Ang
-            RootPart.Velocity = Vector3.new(9e7, 9e7, 9e7)
+            Character:SetPrimaryPartCFrame(CFrame.new(BasePart.Position) * Pos * Ang)
+            RootPart.Velocity = Vector3.new(9e7, 9e7 * 10, 9e7)
             RootPart.RotVelocity = Vector3.new(9e8, 9e8, 9e8)
         end
+        
         local SFBasePart = function(BasePart)
-            local TimeToWait = 1.5
+            local TimeToWait = 2
             local Time = tick()
             local Angle = 0
 
             repeat
                 if RootPart and THumanoid then
-                    if BasePart.Velocity.Magnitude < 30 then
-                        Angle = Angle + 50
+                    if BasePart.Velocity.Magnitude < 50 then
+                        Angle = Angle + 100
 
                         FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle),0 ,0))
                         task.wait()
@@ -117,6 +121,15 @@ local SkidFling = function(TargetPlayer)
 
                         FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
+                        
+                        FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
+                        task.wait()
+
+                        FPos(BasePart, CFrame.new(0, -1.5, -TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(0, 0, 0))
+                        task.wait()
+
+                        FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
+                        task.wait()
 
                         FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(math.rad(90), 0, 0))
                         task.wait()
@@ -135,11 +148,17 @@ local SkidFling = function(TargetPlayer)
                 end
             until BasePart.Velocity.Magnitude > 500 or BasePart.Parent ~= TargetPlayer.Character or TargetPlayer.Parent ~= Players or not TargetPlayer.Character == TCharacter or THumanoid.Sit or Humanoid.Health <= 0 or tick() > Time + TimeToWait
         end
+        
         workspace.FallenPartsDestroyHeight = 0/0
+        
         local BV = Instance.new("BodyVelocity")
+        BV.Name = "EpixVel"
         BV.Parent = RootPart
         BV.Velocity = Vector3.new(9e8, 9e8, 9e8)
         BV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
+        
+        Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
+        
         if TRootPart and THead then
             if (TRootPart.CFrame.p - THead.CFrame.p).Magnitude > 5 then
                 SFBasePart(THead)
@@ -155,10 +174,14 @@ local SkidFling = function(TargetPlayer)
         else
             return Message("Error Occurred", "Target is missing everything", 5)
         end
+        
         BV:Destroy()
+        Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
         workspace.CurrentCamera.CameraSubject = Humanoid
+        
         repeat
             RootPart.CFrame = getgenv().OldPos * CFrame.new(0, .5, 0)
+            Character:SetPrimaryPartCFrame(getgenv().OldPos * CFrame.new(0, .5, 0))
             Humanoid:ChangeState("GettingUp")
             table.foreach(Character:GetChildren(), function(_, x)
                 if x:IsA("BasePart") then
