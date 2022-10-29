1b82fcacd56d2b85bd77e675e8b39d272995eec8
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2022-10-29 10:21:18 -0400

    Update ChatBypass.lua

--- scripts/ChatBypass.lua
+++ scripts/ChatBypass.lua
@@ -1,33 +1,9 @@
--- Unpatched with fn XD
-
+-- Easy unpatch with fn XD
 -- Execute and say bad words to your hearts content! You can also add more if you know a bit of lua and how this works lol
 -- Made by AnthonyIsntHere and DecayedK!
 -- Anyone who says otherwise is stupid :p
-local Bypasses = {
-    ["ass"] = "as{{aieixzvzx:s}}",
-    ["asshole"] = "as{{aieixzvzx:shole}}",
-    ["bitch"] = "bit{{aieixzvzx:ch}}",
-    ["cock"] = "co{{aieixzvzx:ck}}",
-    ["cunt"] = "cu{{aieixzvzx:nt}}",
-    ["dick"] = "di{{zczxczvz:ck}}",
-    ["dyke"] = "dy{{aieixzvzx:ke}}",
-    ["faggot"] = "fa{{aieixzvzx:ggot}}",
-    ["fuck"] = "fu{{aieixzvzx:ck}}",
-    ["gaylord"] = "gayl{{aieixzvzx:ord}}",
-    ["kike"] = "ki{{aieixzvzx:ke}}",
-    ["motherfucker"] = "motherf{{aieixzvzx:ucker}}",
-    ["nigga"] = "ni{{aieixzvzx:gga}}",
-    ["nigger"] = "ni{{aieixzvzx:gger}}",
-    ["piss"] = "p{{aieixzvzx:iss}}",
-    ["penis"] = "pe{{aieixzvzx:nis}}",
-    ["pussy"] = "pu{{aieixzvzx:ssy}}",
-    ["shit"] = "sh{{aieixzvzx:it}}",
-    ["slut"] = "sl{{aieixzvzx:ut}}",
-    ["whore"] = "who{{aieixzvzx:re}}",
-    ["discord"] = "disco{{aieixzvzx:rd}}",
-    ["kys"] = "k{{aieixzvzx:ys}}",
-    ["kill"] = "ki{{aieixzvzx:ll}}"
-}
+
+local Bypasses = loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/misc/Bypasses.lua"))()
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Remote = ReplicatedStorage:FindFirstChild("SayMessageRequest", true)
