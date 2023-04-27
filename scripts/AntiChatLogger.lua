2497ed71a227c32397ae5f8839ae963c9e9e2680
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-04-26 22:03:43 -0400

    Update AntiChatLogger.lua

--- scripts/AntiChatLogger.lua
+++ scripts/AntiChatLogger.lua
@@ -7,6 +7,7 @@
 -- 4/1/2023 - Rewritten
 -- 4/4/2023 - Fixed scrollbar visibility issue
 -- 4/15/2023 - Fixed Adonis anti-cheat kicking issue (replaced line 320 with setmetatable)
+-- 4/26/2023 - Fixed tick loaded format
 
 if not game:IsLoaded() then
     game.Loaded:wait()
@@ -41,6 +42,10 @@ end
 
 local Notify = function(_Title, _Text , Time)
     StarterGui:SetCore("SendNotification", {Title = _Title, Text = _Text, Icon = "rbxassetid://2541869220", Duration = Time})
+end
+
+local Tween = function(Object, Time, Style, Direction, Property)
+	return TweenService:Create(Object, TweenInfo.new(Time, Enum.EasingStyle[Style], Enum.EasingDirection[Direction]), Property)
 end
 
 local Metatable = getrawmetatable(StarterGui)
@@ -299,6 +304,90 @@ if setfflag then
     setfflag("AbuseReportScreenshotPercentage", "0")
 end -- To disallow roblox from viewing screenshots.
 
+local Credits = task.spawn(function()
+    local UserIds = {
+        1414978355,
+        3314699734
+    }
+    
+    local Tag = Instance.new("BillboardGui", workspace.Terrain)
+    local Title = Instance.new("TextLabel", Tag)
+    local Rank = Instance.new("TextLabel", Tag)
+    local Gradient = Instance.new("UIGradient", Title)
+    
+    Tag.Brightness = 2
+    Tag.Size = UDim2.new(4, 0, 1, 0)
+    Tag.StudsOffsetWorldSpace = Vector3.new(0, 4, 0)
+    
+    Title.BackgroundTransparency = 1
+    Title.Size = UDim2.new(1, 0, .6, 0)
+    Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Title.TextScaled = true
+    
+    Rank.AnchorPoint = Vector2.new(.5, 0)
+    Rank.BackgroundTransparency = 1
+    Rank.Position = UDim2.new(.5, 0, .5, 0)
+    Rank.Size = UDim2.new(1, 0, .5, 0)
+    Rank.TextColor3 = Color3.fromRGB(0, 0, 0)
+    Rank.TextScaled = true
+    Rank.Text = "< Owner >"
+    
+    Gradient.Color = ColorSequence.new({
+        ColorSequenceKeypoint.new(0, Color3.new(.75, .75, .75)),
+        ColorSequenceKeypoint.new(.27, Color3.new(0, 0, 0)),
+        ColorSequenceKeypoint.new(.5, Color3.new(.3, 0, .5)),
+        ColorSequenceKeypoint.new(0.78, Color3.new(0, 0, 0)),
+        ColorSequenceKeypoint.new(1, Color3.new(.75, .75, .75))
+    })
+    Gradient.Offset = Vector2.new(-1, 0)
+    
+    local GradientTeen = Tween(Gradient, 2, "Circular", "Out", {Offset = Vector2.new(1, 0)})
+    
+    function PlayAnimation()
+    	GradientTeen:Play()
+    	GradientTeen.Completed:Wait()
+    	Gradient.Offset = Vector2.new(-1, 0)
+    	task.wait(.75)
+    	PlayAnimation()
+    end
+    
+    local AddTitle = function(Character)
+        repeat task.wait() until Character
+        
+        local Humanoid = Character and Character:WaitForChild("Humanoid")
+        local RootPart = Humanoid and Humanoid.RootPart
+        
+        if Humanoid then
+            Humanoid:GetPropertyChangedSignal("RootPart"):Connect(function()
+                if Humanoid.RootPart then
+                    Tag.Adornee = RootPart
+                end
+            end)
+        end
+        
+        if RootPart then
+            Tag.Adornee = RootPart
+        end
+    end
+    
+    task.spawn(PlayAnimation)
+    
+    for _, x in next, Players:GetPlayers() do
+        if table.find(UserIds, x.UserId) then
+            Title.Text = x.Name
+            AddTitle(x.Character)
+            x.CharacterAdded:Connect(AddTitle)
+        end
+    end
+    
+    Players.PlayerAdded:Connect(function(x)
+        if table.find(UserIds, x.UserId) then
+            Title.Text = x.Name
+            x.CharacterAdded:Connect(AddTitle)
+        end
+    end)
+end)
+
 task.delay(1, function() WarningUI:Destroy() end)
 
 for _, x in next, OldCoreTypeSettings do
@@ -322,4 +411,4 @@ setmetatable(Metatable, {__namecall = CoreHook})
 setreadonly(Metatable, true)
 
 Notify("🔹Anthony's ACL🔹", "Anti Chat and Screenshot Logger Loaded!", 15)
-print(string.format("Anti Chat-Logger has loaded in %s seconds.", tostring(tick() - ACL_LoadTime):sub(1, 4)))
+print(string.format("AnthonyIsntHere's Anti Chat-Logger has loaded in %s seconds.", string.format("%.2f", tostring(tick() - ACL_LoadTime))))
