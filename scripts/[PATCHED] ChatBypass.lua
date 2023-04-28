863a6fe9154aa603895820d1bc07a80a66c7bdc2
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-04-28 15:11:33 -0400

    Update and rename ChatBypass.lua to [PATCHED] ChatBypass.lua

--- scripts/[PATCHED] ChatBypass.lua
+++ scripts/[PATCHED] ChatBypass.lua
@@ -1,7 +1,5 @@
--- Easy unpatch with fn XD
--- Execute and say bad words to your hearts content! You can also add more if you know a bit of lua and how this works lol
--- Made by AnthonyIsntHere and Not Scary!
--- Anyone who says otherwise is stupid :p
+-- Patched
+-- Made by AnthonyIsntHere and Decayed
 
 local Bypasses = loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/misc/Bypasses.lua"))()
 
