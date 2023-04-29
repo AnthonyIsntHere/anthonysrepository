6044b41025b2b722db76fd203c6e41cb13ae91b2
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-04-29 12:50:59 -0400

    Create AntiChatLag.lua

---
+++ scripts/AntiChatLag.lua
@@ -0,0 +1,57 @@
+-- Made by AnthonyIsntHere
+if not game:IsLoaded() then
+    game.Loaded:wait()
+end
+
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer
+local PlayerGui = Player.PlayerGui
+
+local Chat = PlayerGui:FindFirstChild("Chat") 
+local MessageDisplay = Chat and Chat:FindFirstChild("Frame_MessageLogDisplay", true)
+local Scroller = MessageDisplay and MessageDisplay:FindFirstChild("Scroller")
+
+local Gsub = string.gsub
+local Lower = string.lower
+
+if not Scroller then return end
+
+for _, x in next, Scroller:GetChildren() do
+    local MessageTextLabel = x:FindFirstChildWhichIsA("TextLabel")
+        
+    if MessageTextLabel then
+        local Message = Gsub(MessageTextLabel.Text, "^%s+", "")
+        
+        if Message:match(" ") then
+            x:Destroy()
+        end
+    end
+end
+
+local ChatAdded = Scroller.ChildAdded:Connect(function(x)
+    local MessageTextLabel = x:FindFirstChildWhichIsA("TextLabel")
+    local SenderTextButton = MessageTextLabel and MessageTextLabel:FindFirstChildWhichIsA("TextButton")
+    if MessageTextLabel then
+        repeat task.wait() until not MessageTextLabel.Text:match("__+")
+
+        local Message = Gsub(MessageTextLabel.Text, "^%s+", "")
+        local Sender = Gsub(SenderTextButton.Text, "[%[%]:]", "")
+
+        if Players:FindFirstChild(Sender) then
+            Sender = Players:FindFirstChild(Sender)
+        else
+            for _,x in next, Players:GetPlayers() do
+                if Sender == x.DisplayName then
+                    Sender = x
+                end
+            end
+        end
+
+        if type(Sender) == "string" then return end
+        
+        if Message:match(" ") then
+            x:Destroy()
+        end
+    end
+end)
