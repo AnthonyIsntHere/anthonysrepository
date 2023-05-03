e710e62add3ba617fde180a6ab96a2c50c5bee20
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-05-03 08:01:24 -0400

    Update and rename [PATCHED] Backpack play.lua to Backpack Play for BoomBox.lua

--- scripts/Backpack Play for BoomBox.lua
+++ scripts/Backpack Play for BoomBox.lua
@@ -1,14 +1,12 @@
--- This script still works, it's just that roblox has patched almost every working audio that can be played by boombox lol.
+-- Made by AnthonyIsntHere
 local ID = 694201337
-
-local AssetVersionId = game:HttpGet("https://www.roblox.com/studio/plugins/info?assetId="..ID):match("value=.(%d+)")
 
 local Settings = {
     Massplay = {
-        Status = true; 
-        MaxAmount = 10;
-    };
-    Time = 0;
+        Status = true,
+        MaxAmount = 10,
+    },
+    Time = 0
 }
 
 local Players = game:GetService("Players")
@@ -25,13 +23,13 @@ local BoomBoxes = {}
 
 local If_BoomBox = false
 
-local Message = function(MTitle ,MText, Time)
+local Message = function(MTitle, MText, Time)
     StarterGui:SetCore("SendNotification",
         {
-            Title = MTitle;
-            Text = MText;
-            Icon = "rbxassetid://2541869220";
-            Duration = Time;
+            Title = MTitle,
+            Text = MText,
+            Icon = "rbxassetid://2541869220",
+            Duration = Time
         }
     )
 end
@@ -56,17 +54,17 @@ if not Humanoid then return Message("Error:", "No Humanoid Found!", 5) end Human
 GetBoomBoxes() if not If_BoomBox then return Message("Error:", "No BoomBoxes Found!", 5) end
 
 table.foreach(BoomBoxes, function(_, x)
-    coroutine.wrap(function()
+    task.spawn(function()
         x.Parent = Character
         local Remote = x:FindFirstChildWhichIsA("RemoteEvent", true)
         if Remote then
-            Remote:FireServer("PlaySong", "&assetversionid="..AssetVersionId)
+            Remote:FireServer("PlaySong", ID)
         end
-        x.DescendantAdded:wait(); task.wait();
+        x.DescendantAdded:wait() task.wait()
         x.Parent = Backpack
         local Sound = x:FindFirstChildWhichIsA("Sound", true)
         repeat task.wait() until not Sound.IsPlaying
         Sound:Play()
         Sound.TimePosition = Settings.Time
-    end)()
+    end)
 end)
