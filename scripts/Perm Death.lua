3226a344c6a7bf729f81fe4b378224667e4c82a4
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-07-14 14:53:34 -0400

    Update Perm Death.lua

--- scripts/Perm Death.lua
+++ scripts/Perm Death.lua
@@ -2,8 +2,13 @@ local Players = game:GetService("Players")
 
 local Player = Players.LocalPlayer
 local Character = Player.Character ~= nil and Player.Character or workspace:FindFirstChild(tostring(Player), true)
+local Humanoid = Character and Character:FindFirstChildWhichIsA("Humanoid")
+
+if not Humanoid then
+    return
+end
 
 Player.Character = nil
 Player.Character = Character
-wait(Players.RespawnTime + .1) -- If doesn't work first time, extend .1 to .5
-Character:BreakJoints() -- Kill method can be replaced if needed.
+wait(Players.RespawnTime + .1) -- If it doesn't work the first time, extend .1 to .5/higher value
+Humanoid.Health = 0
