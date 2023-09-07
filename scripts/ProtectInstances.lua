10a771e487e7303cfddfd6bc7a57a70b0053059f
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-09-07 19:51:42 -0400

    Create ProtectInstances.lua

---
+++ scripts/ProtectInstances.lua
@@ -0,0 +1,33 @@
+-- Made by AnthonyIsntHere
+-- Basically this just allows you to hide stuff on your client
+-- Common use: bypass any client anti-cheat fly/fling detection (hides bodymovers)
+
+local Protected_Instances = {}
+
+local ProtectHook; ProtectHook = hookmetamethod(game, "__namecall", function(self, ...)
+    if table.find(Protected_Instances, self) and not checkcaller() then
+        return nil
+    end
+    
+    return ProtectHook(self, ...)
+end)
+
+local ClassNameHook; ClassNameHook = hookmetamethod(game, "__index", function(self, index)
+    if index == "ClassName" and not checkcaller() then
+        return ""
+    end
+
+    return ClassNameHook(self, index)
+end)
+
+local InstanceHook; InstanceHook = hookfunction(Instance.new, function(...)
+    local Arguments = {...}
+
+    if checkcaller() and Arguments[1] then
+        local CurrentInst = InstanceHook(...)
+        table.insert(Protected_Instances, CurrentInst)
+        return CurrentInst
+    end
+
+    return InstanceHook(...)
+end)
