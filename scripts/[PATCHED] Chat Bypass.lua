2efafeffd1c30be272e1a8177fed8cad18ed415d
AnthonyIsntHere <78816096+AnthonyIsntHere@users.noreply.github.com>
2023-09-19 22:42:43 -0400

    Update [PATCHED] Chat Bypass.lua

--- scripts/[PATCHED] Chat Bypass.lua
+++ scripts/[PATCHED] Chat Bypass.lua
@@ -2,7 +2,7 @@
 -- Script made by AnthonyIsntHere
 -- Method made by cam1494
 
--- Maintained by Anthony, cam1494, Edgar Zomboss, ionizedparticle, grudging
+-- Maintained by Anthony, cam1494
 -- Old italic unicode block + bait unicodes + cyrillic
 
 -- have fun skids!
